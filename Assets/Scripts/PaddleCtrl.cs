c1f964e4d2fc5a03ed88e05ecd49abb49b040106
j_park <>
2023-01-12 18:05:49 +0900

    #13 공의 움직임을 바꿔보기

--- Assets/Scripts/PaddleCtrl.cs
+++ Assets/Scripts/PaddleCtrl.cs
@@ -5,12 +5,22 @@ using UnityEngine;
 public class PaddleCtrl : MonoBehaviour
 {
     private Vector2 playerPos = new Vector2(0f, -4f); // Paddle의 초기 위치
+
+    private Rigidbody2D ballRigidBody = null; // 공의 리지드 바디
     
     private void OnTriggerEnter2D(Collider2D other)
     {
         Debug.Log ("Touch Paddle");
         Debug.Log ($"paddle Position.x : {playerPos.x}");
         Debug.Log ($"ball Position on Paddle.x : {(other.transform.position.x) - (playerPos.x)}");
+        ballRigidBody = other.GetComponent<Rigidbody2D>();
+        Debug.Log ($"ballRigidBody Vector = {ballRigidBody.GetVector(playerPos)}");
+
+        Vector2 inDirection = ballRigidBody.velocity; // 입사 벡터 (속도)
+        Vector2 inNormal = transform.up; // 노말 벡터
+        Vector2 result = Vector2.Reflect(inDirection, inNormal);
+
+        ballRigidBody.velocity = result;
     }
 
     // Update is called once per frame
