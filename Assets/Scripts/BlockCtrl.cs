b3f3768c28a6b0bf122faa576aecd467384eaf19
j_park <>
2023-01-17 18:02:47 +0900

    #13 block 쪽의 스크립트에 붙어있던 공 반사 코드를 ball 쪽으로 이동

--- Assets/Scripts/BlockCtrl.cs
+++ Assets/Scripts/BlockCtrl.cs
@@ -9,19 +9,15 @@ public class BlockCtrl : MonoBehaviour
     // 공과 블럭이 충돌할 때
     private void OnCollisionEnter2D(Collision2D other)
     {
-        // 블럭에 접촉한 공의 정보 가져오기
-        Rigidbody2D ballRigidBody = other.collider.gameObject.GetComponent<Rigidbody2D>();
-
         if (brickParticle != null)
         {
-            Vector2 blockPos = gameObject.transform.position;
-            
-            Debug.Log ("Break Block");
-            Debug.Log ($"ballRigidBody Block Vector = {ballRigidBody.GetVector(blockPos)}");
             // 이펙트를 발생시킨다
             Instantiate(brickParticle, transform.position, Quaternion.identity);
             
+            // GameManager의 DestroyBrick 함수 실행
             GameManager.Instance.DestroyBrick();
+            
+            // block Game Object 제거
             Destroy(gameObject);
         }
     }
