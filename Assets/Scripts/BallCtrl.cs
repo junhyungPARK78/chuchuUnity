a8cf7508491f65ad03a4ae7ab6a6c33c8c7dcb99
j_park <>
2023-01-17 18:05:45 +0900

    #13 볼이 접촉하는 물체에 대한 반응에 대한 코드를 전부 ball 쪽의 스크립트로 이동

--- Assets/Scripts/BallCtrl.cs
+++ Assets/Scripts/BallCtrl.cs
@@ -19,6 +19,54 @@ public class BallCtrl : MonoBehaviour
         ballRigidBody = GetComponent<Rigidbody2D>();
     }
 
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.tag == "Paddle")
+        {
+            // debug 정보 모음
+            Debug.Log (@$"
+            ====== Ball과 Paddle 관련 Log 시작 ======
+            ・Paddle에 접촉했습니다.
+            ・paddle Position.x : {other.transform.position.x}
+            ・Ball Position.x : {gameObject.transform.position.x}
+            ・ball Position on Paddle.x : {(gameObject.transform.position.x) - (other.transform.position.x)}
+            ・ballRigidBody Vector = {ballRigidBody.GetVector(gameObject.transform.position)}
+            ====== Log 종료 ======");
+
+            // 공 반사시키기
+            Vector2 inDirection = ballRigidBody.velocity; // 입사 벡터 (속도)
+            Vector2 inNormal = transform.up; // 노말 벡터
+            Vector2 result = Vector2.Reflect(inDirection, inNormal);
+
+            ballRigidBody.velocity = result;
+        }
+
+        if (other.tag == "DeadZone")
+        {
+            // debug 정보 모음
+            Debug.Log (@$"
+            ====== Ball과 DeadZone 관련 Log 시작 ======
+            ・DeadZone에 접촉했습니다.
+            ====== Log 종료 ======");
+
+            // 공 제거하기
+            Destroy(gameObject);
+        }
+    }
+
+    private void OnCollisionEnter2D(Collision2D other)
+    {
+        if (other.gameObject.CompareTag("Block"))
+        {
+            // debug 정보 모음
+            Debug.Log (@$"
+            ====== Ball과 Block 관련 Log 시작 ======
+            ・Break Block
+            ・ballRigidBody Block Vector = {ballRigidBody.GetVector(other.transform.position)}
+            ====== Log 종료 ======");
+        }
+    }
+
     // Update is called once per frame
     void Update()
     {
