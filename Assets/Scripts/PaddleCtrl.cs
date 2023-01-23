7d106dafd03d2346eebbf2b0cc474c707ec9eccb
j_park <>
2023-01-23 18:06:29 +0900

    #43 패들의 크기, 기본 위치 조정 - script

--- Assets/Scripts/PaddleCtrl.cs
+++ Assets/Scripts/PaddleCtrl.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 
 public class PaddleCtrl : MonoBehaviour
 {
-    private Vector2 playerPos = new Vector2(0f, -4f); // Paddle의 초기 위치
+    private Vector2 playerPos = new Vector2(0f, -4.16f); // Paddle의 초기 위치
 
     // Update is called once per frame
     void Update()
@@ -16,7 +16,7 @@ public class PaddleCtrl : MonoBehaviour
         float xPos = Camera.main.ScreenToWorldPoint(cameraPosition).x;
 
         // Paddle 이동 제한
-        playerPos = new Vector2(Mathf.Clamp(xPos, -3.66f, 3.66f), -4f);
+        playerPos = new Vector2(Mathf.Clamp(xPos, -3.66f, 3.66f), -4.16f);
         transform.position = playerPos;
     }
 }
