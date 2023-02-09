1a7fa98a7818d62153ecbba5c24a778da54b53a3
j_park <>
2023-02-09 17:10:01 +0900

    #39 실험을 위해 생명 수를 일단 99개로 늘림

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -28,7 +28,7 @@ public class GameManager : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        lives = 10;
+        lives = 99;
         bricks = 32;
 
         if (txtLives != null)
