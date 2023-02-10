4041c5d21b2385435f5b55715c571f7aad771715
j_park <>
2023-02-10 19:21:29 +0900

    #39 일반 vector와 normalize 한 vector의 각도를 볼 수 있도록 하고 얼마나 오차가 있는지 실험
    실험 성공
    https://github.com/junhyungPARK78/chuchuUnity/issues/39#issuecomment-1425381266

--- Assets/Scripts/Z_TEST.cs
+++ Assets/Scripts/Z_TEST.cs
@@ -4,26 +4,56 @@ using UnityEngine;
 
 public class Z_TEST : MonoBehaviour
 {
+    private float q = 0.0f;
+    private List<int> nums = new List<int>();
+    private int sampleNum = 10;
+
     void Start()
     {
+        for (int i = sampleNum * -1; i <= sampleNum; i++)
+        {
+            nums.Add(i);
+        }
+        // Debug.Log (nums[2]);
+        
         // draw a 5-unit white line from the origin for 2.5 seconds
         // Debug.DrawLine(Vector3.zero, new Vector3(5, 0, 0), Color.white, 2.5f);
-        Debug.DrawLine(Vector3.zero, new Vector3(-5, -3, 0), Color.red);
+        // Debug.DrawLine(Vector3.zero, new Vector3(5, 3, 0), Color.red);
+
+        // List<int> nums = new List<int>(new int[] {10, 0, -10});
+        // foreach (int numX in nums)
+        // {
+        //     foreach (int numY in nums)
+        //     {
+        //         // Debug.DrawLine(Vector3.zero, new Vector3(numX, numY, 0), Color.red);
+        //         Debug.Log ($"numX = {numX} / numY = {numY}");
+        //     }
+        // }
+        
     }
-
-    private float q = 0.0f;
-
+    
     void FixedUpdate()
     {
-        // always draw a 5-unit colored line from the origin
-        Color color = new Color(q, q, 1.0f);
-        Debug.DrawLine(Vector3.zero, new Vector3(5, 5, 0), color);
-        q = q + 0.01f;
-
-        if (q > 1.0f)
+        foreach (int numX in nums)
         {
-            q = 0.0f;
+            foreach (int numY in nums)
+            {
+                Debug.DrawLine(Vector2.zero, new Vector2(numX, numY), Color.red);
+                Debug.DrawLine(Vector2.zero, new Vector2(numX, numY).normalized, Color.green);
+            }
         }
+
+        
+
+        // // always draw a 5-unit colored line from the origin
+        // Color color = new Color(q, q, 1.0f);
+        // Debug.DrawLine(Vector3.zero, new Vector3(5, 5, 0), color);
+        // q = q + 0.01f;
+
+        // if (q > 1.0f)
+        // {
+        //     q = 0.0f;
+        // }
     }
 
     // Event callback example: Debug-draw all contact points and normals for 2 seconds.
