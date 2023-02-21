760d3d1545c70477f029bc342ec9bb4b392a2117
j_park <>
2023-02-21 19:21:24 +0900

    #39 vector 의 각도를 검출하는 방법 알아보기

--- Assets/Scripts/Z_TEST.cs
+++ Assets/Scripts/Z_TEST.cs
@@ -4,18 +4,66 @@ using UnityEngine;
 
 public class Z_TEST : MonoBehaviour
 {
-    private List<int> nums = new List<int>();
-    private int sampleNum = 90;
+    // private List<int> nums = new List<int>();
+    // private int sampleNum = 90;
     private Vector3 ballReflectVector;
+    
+    // 두 vector의 각도 구하기 START
+    public float testAngle;
+    private Vector2 vector1;
+    private Vector2 vector2;
+    private Vector2 vector3;
+    // 두 vector의 각도 구하기 END
 
     void Start()
     {
-        for (int i = sampleNum * -1; i <= sampleNum; i++)
+        // 두 vector의 각도 구하기 START
+        testAngle = 0.0f;
+        vector1 = Vector2.right;
+        vector2 = new Vector2(-1, 1);
+        int testCount = 100000;
+
+        // System.Diagnostics.Stopwatch watch1 = new System.Diagnostics.Stopwatch();
+        // watch1.Start();
+        // watch1.Stop();
+        // Debug.Log($"Vector2.SignedAngle : {watch1.ElapsedMilliseconds} ms");
+
+
+        System.Diagnostics.Stopwatch watch1 = new System.Diagnostics.Stopwatch();
+        watch1.Start();
+        for (int i = 0; i < testCount; ++i)
         {
-            nums.Add(i);
+            testAngle = Vector2.SignedAngle(vector1, vector2) + i;            
         }
+        watch1.Stop();
+        Debug.Log($"Vector2.SignedAngle : {watch1.ElapsedMilliseconds} ms");
+
+        System.Diagnostics.Stopwatch watch2 = new System.Diagnostics.Stopwatch();
+        watch2.Start();
+        for (int i = 0; i < testCount; ++i)
+        {
+            testAngle = Mathf.Atan2(vector2.y, vector2.x) * Mathf.Rad2Deg + i;
+        }
+        watch2.Stop();
+        Debug.Log($"Mathf.Atan2 : {watch2.ElapsedMilliseconds} ms");
+
 
         
+        
+
+        // testAngle = Vector2.SignedAngle(vector1, vector2);
+        // testAngle = Mathf.Atan2(vector2.y, vector2.x) * Mathf.Rad2Deg;
+        // Debug.Log($"testAngle : {testAngle}");
+        // 두 vector의 각도 구하기 END
+
+
+
+
+
+        // for (int i = sampleNum * -1; i <= sampleNum; i++)
+        // {
+        //     nums.Add(i);
+        // }        
 
         // Debug.Log (nums[2]);
         
@@ -37,19 +85,27 @@ public class Z_TEST : MonoBehaviour
     
     void FixedUpdate()
     {
-        Debug.DrawLine(Vector2.zero, Vector2.right * 10, Color.red);
-        // Debug.DrawLine(Vector2.zero, ballReflectVector * 10, Color.blue);
+        // 두 vector의 각도 구하기 START
+        vector2 = Quaternion.AngleAxis(testAngle, Vector3.forward) * vector1;
+        vector3 = Quaternion.AngleAxis(testAngle * -1f, Vector3.forward) * vector1;
+        Debug.DrawLine(Vector2.zero, vector1 * 10, Color.red);
+        Debug.DrawLine(Vector2.zero, vector2 * 10, Color.green);
+        Debug.DrawLine(Vector2.zero, vector3 * 10, Color.blue);
+        // 두 vector의 각도 구하기 END
+        
+        // Debug.DrawLine(Vector2.zero, Vector2.right * 10, Color.red);
+        // // Debug.DrawLine(Vector2.zero, ballReflectVector * 10, Color.blue);
 
-        foreach (int numX in nums)
-        {
-            // foreach (int numY in nums)
-            // {
-            //     Debug.DrawLine(Vector2.zero, new Vector2(numX, numY), Color.red);
-            //     Debug.DrawLine(Vector2.zero, new Vector2(numX, numY).normalized, Color.green);
-            // }
-            ballReflectVector = Quaternion.AngleAxis(numX + 90, Vector3.forward) * Vector2.right;
-            Debug.DrawLine(Vector2.zero, ballReflectVector, Color.green);
-        }
+        // foreach (int numX in nums)
+        // {
+        //     // foreach (int numY in nums)
+        //     // {
+        //     //     Debug.DrawLine(Vector2.zero, new Vector2(numX, numY), Color.red);
+        //     //     Debug.DrawLine(Vector2.zero, new Vector2(numX, numY).normalized, Color.green);
+        //     // }
+        //     ballReflectVector = Quaternion.AngleAxis(numX + 90, Vector3.forward) * Vector2.right;
+        //     Debug.DrawLine(Vector2.zero, ballReflectVector, Color.green);
+        // }
 
         
 
